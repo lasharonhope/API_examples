442b3cd7e36eaea2740caa1b30400fba41531301
LaSharon Hope <123046254+lasharonhope@users.noreply.github.com>
2024-03-24 23:58:50 -0500

    updates

---
+++ app.js
@@ -0,0 +1,20 @@
+const x = document.getElementById("demo");
+
+function getLocation() {
+  try {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } catch {
+    x.innerHTML = err;
+  }
+}
+
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude;
+}
+
+function myFunction() {
+    alert("Wings, I could eat them everyday!");
+  }
+
+  
